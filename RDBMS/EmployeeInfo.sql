6d8caddb46c41a812a0caa4587d8402ce6bc0dcd
MuthuR-Datasirpi <muthu.r@datasirpi.com>
2023-07-13 16:14:31 +0530

    Table, and Word

---
+++ RDBMS/EmployeeInfo.sql
@@ -0,0 +1,7 @@
+use Records;
+insert into EmployeeInfo(EmployeeId, FirstName, LastName, Department)
+values ('1001','Raj','Kumar','HR'),
+('1002','Raj','kamal','Sales man'),
+('1003','Muthu','kumar','Finacee'),
+('1004','Muthu',null,'Developer');
+select * FROM EmployeeInfo;
