c58662092a85b9eae3d0478a3be78d7d5982d239
MuthuR-Datasirpi <muthu.r@datasirpi.com>
2023-07-19 16:24:50 +0530

    int,string,const,final,double,num-Dart variables

---
+++ variables.dart
@@ -0,0 +1,42 @@
+void main(){
+//Declaring Variables
+String name = "John";
+String address = "USA";  
+num age = 20; // used to store any types of numbers 
+num height = 5.9;
+bool isMarried = false;
+
+// printing variables value   
+print("Name is $name");
+print("Address is $address");
+print("Age is $age");
+print("Height is $height");
+print("Married Status is $isMarried");
+
+const pi = 3.14;
+// pi = 4.23; // not possible  redeclartion not possible in const
+print("Value of PI is $pi");
+
+//Naming convention
+// Not standard way
+var fullname = "John Doe";
+// Standard way used lowercamel case
+var fullName = "John Doe";
+const p = 3.14;
+print(fullName);
+
+//final Keyword  final variable_name
+final k=20;
+print(k);
+//or
+//final data_type  variable_name
+final int a=10;
+print(a);
+
+
+
+
+
+
+
+}
