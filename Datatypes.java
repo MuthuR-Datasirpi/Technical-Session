3893121d82ddf519daf3e751cc7c2b5f2edc3459
MuthuR-Datasirpi <muthu.r@datasirpi.com>
2023-07-24 16:10:21 +0530

    Primitive and non primitive datatypes

---
+++ Datatypes.java
@@ -0,0 +1,60 @@
+class Datatypes{
+public static void main(String[] args) {
+//!Primitive datatype
+		// Creating and initializing custom character
+		char a = 'G';
+
+		// Integer data type is generally
+		// used for numeric values
+		int i = 89;
+
+		// use byte and short
+		byte b = 4; //& -128 to 127 */
+
+		// this will give error as number is
+		// larger than byte range
+		// byte b1 = 7888888955;
+
+		short s = 56;  
+
+		// this will give error as number is
+		// larger than short range
+		// short s1 = 87878787878;
+
+		// by default fraction value
+		// is double in java
+		double d = 4.355453532;
+
+		// for float use 'f' as suffix as standard
+		float f = 4.7333434f;
+
+		// need to hold big range of numbers then we need
+		// this data type
+		long l = 12121;
+
+		System.out.println("char: " + a);
+		System.out.println("integer: " + i);
+		System.out.println("byte: " + b);
+		System.out.println("short: " + s);
+		System.out.println("float: " + f);
+		System.out.println("double: " + d);
+		System.out.println("long: " + l);
+
+//!Non-Primitive Data Type or Reference Data Types
+    //~ 1.String
+    //~ 2.class
+    //~ 3.object
+    //~ 4.interface
+    //~ 5.Array
+    // 1. // Declare String without using new operator 
+    String D = "GeeksforGeeks"; 
+    System.out.println(D);
+    // Declare String using new operator 
+    String s1 = new String("GeeksforGeeks");
+    System.out.println(s1); 
+
+
+	}
+}
+
+
