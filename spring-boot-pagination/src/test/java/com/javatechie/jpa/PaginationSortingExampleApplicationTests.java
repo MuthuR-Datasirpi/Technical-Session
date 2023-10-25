8d4b8ab2c4698d1272a1d014e85628dec801e507
Muthu <muthuer6@gmail.com>
2023-10-25 16:02:59 +0530

    completed

---
+++ spring-boot-pagination/src/test/java/com/javatechie/jpa/PaginationSortingExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.javatechie.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationSortingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
