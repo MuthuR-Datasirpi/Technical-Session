daaa27261d660546648b574d058f511ed9f272e6
MuthuR-Datasirpi <muthu.r@datasirpi.com>
2023-12-12 11:51:51 +0530

    fixed

--- Headers/src/main/java/com/Project/SpringCrud/SpringCrudApplication.java
+++ Headers/src/main/java/com/Project/SpringCrud/SpringCrudApplication.java
@@ -1,10 +1,10 @@
 package com.Project.SpringCrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-//@EnableSwagger2
+
 public class SpringCrudApplication
 {
 	public static void main(String[] args)
